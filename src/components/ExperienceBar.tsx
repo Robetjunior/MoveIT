c477aab58c7369f7df8a30dcaba0039e25c5d2fa
Jose Roberto <juniorferreirarob@gmail.com>
2021-02-24 20:28:19 -0300

    create regra de negocios

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,15 +1,21 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar(){
+  const {currentExperience, experienceToNextLevel} = useContext(ChallengeContext)
+
+  const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel
+
   return(
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{maxWidth: '50%'}}>
-          <span className={styles.currentExperience} style={{left: '50%'}}>300 xp</span>
+        <div style={{maxWidth: `${percentToNextLevel}%`}}>
+          <span className={styles.currentExperience} style={{left: `${percentToNextLevel}%`}}>{currentExperience} xp</span>
         </div>
       </div>
-      <span>600 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </header>
   );
 }
